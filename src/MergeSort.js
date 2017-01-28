7404b14c12d5276ec496febbaca441cd7622bf00
aniket sharma <aniket965.as@gmail.com>
2017-01-28 13:52:16 +0530

    solved merge sort

--- src/MergeSort.js
+++ src/MergeSort.js
@@ -1,51 +1,58 @@
 var  a =[2,15,4366,4,123,32,4];
-//for merging elements
+
 function Merge(array,left,right,middle){
     n1 = middle - left +1;
     n2 = right - middle;
- var  L = [];
+   var  L = [];
    var R = [];
     for(var i =0;i<n1;i++){
-        L[i] = array[left + i];
+        L[i] = array[left + i ];
     }
-    console.log(L);
     for(var j=0;j<n2;j++){
-        R[j]=array[middle+left+j];
+        R[j]=array[middle+j+1];
     }
     
-    i = 0    
-    j = 0    
-    k = left    
- 
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]){
-            array[k] = L[i]
-            i += 1
-        }
-        else{
-            array[k] = R[j]
-            j += 1
-        k += 1
-        }
-    }
-    while( i < n1){
-        array[k] = L[i]
-        i += 1
-        k += 1
-    }
-   
-    while (j < n2){
-        array[k] = R[j]
-        j += 1
-        k += 1
-    }
+    i = 0;    
+    j = 0 ;   
+    var k = left;
+	        while (i < n1 && j < n2)
+	        {
+	            if (L[i] <= R[j])
+	            {
+	                array[k] = L[i];
+	                i++;
+	            }
+	            else
+	            {
+	                array[k] = R[j];
+	                j++;
+	            }
+	            k++;
+	        }
+	 
+	        while (i < n1)
+	        {
+	            array[k] = L[i];
+	            i++;
+	            k++;
+	        }
+	 
+	   while (j < n2)
+	        {
+	            array[k] = R[j];
+	            j++;
+	            k++;
+	        }
+	    }
     
 
-}
+ 
+    
+
 
 function MergeSort(array,left,right){
     if(left < right){
-        var middle = Math.floor((left+right)/2);
+        var middle = Math.floor((left +right)/2);
         MergeSort(array,left,middle);
         MergeSort(array,middle+1,right);
         Merge(array,left,right,middle);
@@ -53,4 +60,4 @@ function MergeSort(array,left,right){
     }
 }
 MergeSort(a,0,a.length-1);
-console.log();
+console.log(a);
